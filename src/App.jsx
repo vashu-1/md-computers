0b1a0511cd1e8c0653face41faa3308960d63829
Vijay Kumar Modak <vijaymodak9945@gmail.com>
2025-04-16 01:32:53 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Navbar from "./Components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Login from "./Components/Login";
-
+import Header from "./Components/Header";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,37 +2,52 @@ import React from "react";
 import { HiOutlineShoppingBag } from "react-icons/hi2";
 import { IoMdSearch } from "react-icons/io";
 import { IoLogInOutline } from "react-icons/io5";
+import { PiSignIn } from "react-icons/pi";
 import { Link } from "react-router-dom";
+import { FaRegHeart } from "react-icons/fa6";
+import { TbArrowsCross } from "react-icons/tb";
 
 function Navbar() {
   return (
-    <nav className="bg-[#0D0E10] p-4">
-      <div className="container  mx-auto flex justify-between items-center">
-        <Link to="/" className="text-white text-lg font-bold">
+    <nav className="bg-secondary bg-sticky p-4">
+      <div className=" container h-full flex justify-between items-center">
+        <Link to="/" className="text-white px-12 text-xl font-bold">
           Brand
         </Link>
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/" className="text-gray-300 hover:text-white">
-              Home
-            </Link>
-          </li>
+
+        <div className="flex text-white border bg-gray-900 px-3 py-1 rounded-lg items-center gap-2">
+          <input
+            type="text"
+            placeholder="Search"
+            className="outline-none w-136"
+          />
+          <button className="bg-gray-900 cursor-pointer text-white px-2 py-1 rounded-full">
+            <IoMdSearch />
+          </button>
+        </div>
+
+        <ul className=" flex px-4 gap-2 text-xl space-x-4">
           <li>
             <Link to="/login" className="text-gray-300 hover:text-white">
-              Login
+              <PiSignIn />
             </Link>
           </li>
           <li>
             <Link to="/about" className="text-gray-300 hover:text-white">
-              About
+              <FaRegHeart />
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-gray-300 hover:text-white">
+              <TbArrowsCross />
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" className="text-gray-300 hover:text-white">
+              <HiOutlineShoppingBag />
             </Link>
           </li>
         </ul>
-        <div className="flex items-center gap-4">
-          <HiOutlineShoppingBag />
-          <IoMdSearch />
-          <IoLogInOutline />
-        </div>
       </div>
     </nav>
   );
