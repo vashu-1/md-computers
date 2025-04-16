bf8b93390f334e4b6ab67a728ec79f0058c8fea2
Vijay Kumar Modak <vijaymodak9945@gmail.com>
2025-04-16 14:15:33 +0530

    modify

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,11 @@ import React from "react";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
-<<<<<<< HEAD
 
 import Footer from "./Components/footer";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
-
-=======
-import Login from "./Components/Login";
 import Header from "./Components/Header";
->>>>>>> 0b1a0511cd1e8c0653face41faa3308960d63829
 function App() {
   return (
     <BrowserRouter>
@@ -20,10 +15,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
