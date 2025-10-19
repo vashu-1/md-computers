a2fd0fb29828af67dd223f86e547fd1d21c0cc85
Vijay Kumar Modak <vijaymodak9945@gmail.com>
2025-10-19 07:13:00 +0530

    Fixed export issues and component imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,26 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./Components/Navbar";
-import Banner from "./Components/Home/Banner";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "./Components/Home";
-
+import Home from "./Components/Home/index";
 import Footer from "./Components/Home/Footer";
-import Login from "./pages/login";
-import Signup from "./pages/signup";
-
 import Header from "./Components/Home/Header";
-import Products from "./Components/Home/Products";
-import ContactUs from "./Components/Home/ContactInfo";
-import MackbookPlus from "./Components/Home/MackbookPlus";
-import Swiper from "./Components/Home/Swiper";
-import Explore from "./Components/Home/Explore";
-import MainCategories from "./Components/Home/MainCategories";
-import WhatYouSayAboutUs from "./Components/Home/WhatYouSayAboutUs";
-import OfferZone from "./Components/Home/OfferZone";
-import Brand from "./Components/Home/Brand";
+
+// Import login and signup components from pages directory
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <Navbar />
-      <Banner />
-      <Swiper/>
-      <OfferZone/>
-      <Products/>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
-      <MackbookPlus/>
-      <Explore/>
-      <MainCategories/>
-      <Brand/>
-      <WhatYouSayAboutUs/>
-      <ContactUs/>
+      <Footer />
       <Footer />
     </BrowserRouter>
   );
